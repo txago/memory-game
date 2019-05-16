8f61c3ec5a6566930f0528b0c2f1d62a6d26639d
Thiago Menezes <5039518+txago@users.noreply.github.com>
2019-05-16 15:04:58 -0300

    Updates game JS and style

--- js/app.js
+++ js/app.js
@@ -1,60 +1,217 @@
-// Cards list array
-const brands = ['behance', 'dribbble', 'facebook', 'instagram', 'linkedin',
-'snapchat', 'twitter', 'youtube'];
-
 // Global vars
-const cardsDeck = document.querySelector('#brand-deck');
+const deck = document.querySelector('.deck');
+const restart = document.querySelector(".restart-button");
+
+// Empty array list that holds all of the cards
+let cardsList = [];
+
+// Matched cards vars
+let matchedCards = 0;
+const totalMatches = 8;
+
+// Moves vars
 let moves = 0;
-const movesCounter = document.querySelector(".moves");
+let movesCount = document.querySelector('.counter');
 
-// Cards arrays
-let flippedCards = [];
-let matchedCards = [];
+// Clock vars
+let timeStart;
+let clockOn = false;
+let hours = 0;
+let hoursLabel = document.querySelector('.hour');
+let minutes = 0;
+let minutesLabel = document.querySelector('.minutes');
+let seconds = 0;
+let secondsLabel = document.querySelector('.seconds');
+const totalTime = document.querySelector('.clock');
 
-// Deck EventListener
-cardsDeck.addEventListener('click', cardClick);
+// Modal vars
+const modal = document.querySelector('.overlay-modal');
+const modalCloseX = document.querySelector('.modal-close');
+const modalButton = document.querySelector('.modal-button');
 
-// Cards interaction
-function cardClick(evt) {
-  let clickedCard = evt.target;
+// Rate vars
+const stars = document.querySelector('.stars').childNodes;
+const starsStats = document.querySelector('.stars');
 
-  if (clickedCard.classList.contains('card') && !clickedCard.classList.contains('open', 'show', 'match')) {
-    clickedCard.classList.add('open', 'show');
-    flippedCards.push(clickedCard);
+// Function to randomize the cards on the deck
+function randomDeck() {
+  const cardsToShuffle = Array.from(document.querySelectorAll('.deck li'));
+  const shuffledCards = shuffle(cardsToShuffle);
+  for (let card of shuffledCards) {
+    deck.appendChild(card);
   }
+}
+randomDeck();
 
-  if (flippedCards.length === 2) {
-    clickNum();
-
-    if (flippedCards[0].innerHTML === flippedCards[1].innerHTML) {
-      // Flipped cards match!
-      matched();
-    } else {
-      // Flipped cards doesn't match!
-      setTimeout(() => {
-        flippedCards[0].classList.remove('open', 'show');
-        flippedCards[1].classList.remove('open', 'show');
-        flippedCards = [];
-      }, 1000);
+// Event Delegation for cards' clicking and matching behaviour
+deck.addEventListener('click', event => {
+  const clickTarget = event.target;
+  if (clickTarget.classList.contains('card') &&
+  !clickTarget.classList.contains('match') && cardsList.length < 2 &&
+  !cardsList.includes(clickTarget)) {
+    flipCard(clickTarget);
+    cardsList.push(clickTarget);
+    if (cardsList.length === 2) {
+      countMoves();
+      matchCards(clickTarget);
+    }
+    if (clockOn === false) {
+      startClock();
+      clockOn = true;
     }
   }
+});
+
+// Function to flip single card
+function flipCard(clickTarget) {
+  clickTarget.classList.toggle('open');
+  clickTarget.classList.toggle('show');
 }
 
-// Flipped cards match!
-function matched() {
-  flippedCards[0].classList.add('match');
-  flippedCards[1].classList.add('match');
-  matchedCards.push(flippedCards[0]);
-  matchedCards.push(flippedCards[1]);
-  flippedCards = [];
+// Function to match cards
+function matchCards() {
+  if (cardsList[0].innerHTML === cardsList[1].innerHTML) {
+    cardsList[0].classList.toggle('match');
+    cardsList[1].classList.toggle('match');
+    cardsList = [];
+    matchedCards++;
+    if (matchedCards === totalMatches) {
+      completeGame();
+    }
+  } else {
+    setTimeout(() => {
+      flipCard(cardsList[0]);
+      flipCard(cardsList[1]);
+      cardsList = [];
+    }, 800);
+  }
 }
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// Function to count the moves
+function countMoves() {
+    moves++;
+    if (moves === 1) {
+        movesCount.innerHTML = '1 move';
+    } else {
+        movesCount.innerHTML = `${moves} moves`;
+    }
+    starsRating();
+}
+
+// Function to set the stars rating
+function starsRating() {
+  if (moves === 6) {
+    stars[9].innerHTML = '<i class="far fa-star">';
+  } else if (moves === 12) {
+    stars[7].innerHTML = '<i class="far fa-star">';
+  } else if (moves === 24) {
+    stars[5].innerHTML = '<i class="far fa-star">';
+  } else if (moves === 32) {
+    stars[3].innerHTML = '<i class="far fa-star">';
+  } else if (moves === 44) {
+    stars[1].innerHTML = '<i class="far fa-star">';
+  }
+}
+
+// Function to start the clock
+function startClock() {
+  if (seconds === 0) {
+    seconds++;
+  }
+  timeStart = setInterval(() => {
+    hoursLabel.innerHTML = `${hours}`;
+    minutesLabel.innerHTML = `${minutes}`;
+    secondsLabel.innerHTML = `${seconds}`;
+    fixClock(seconds, secondsLabel);
+    fixClock(minutes, minutesLabel);
+    fixClock(hours, hoursLabel);
+    seconds++;
+    if (seconds == 60) {
+      minutes++;
+      seconds = 0;
+    } else if (minutes == 60) {
+      hours++;
+      minutes = 0;
+    }
+  }, 1000);
+}
+
+// Function to fix clock format
+function fixClock(x, y) {
+    if (x < 10) {
+        return (y.innerHTML = `0${x}`);
+    } else {
+        return (y.innerHTML = `${x}`);
+    }
+}
+
+// Event delegation to restart the board from restart link
+restart.addEventListener('click', restartGame);
+
+// Function to complete the game
+function completeGame() {
+  clearInterval(timeStart);
+  modalStats();
+  openModal();
+}
+
+// Function to diplay winning modal
+function openModal() {
+  const modal = document.querySelector('.overlay-modal');
+  modal.removeAttribute('style');
+}
+openModal();
+
+// Function to close winning modal
+function closeModal() {
+  const modal = document.querySelector('.overlay-modal');
+  modal.setAttribute('style', 'display: none;');
+}
+closeModal();
+
+// Function to display stats to winning modal
+function modalStats() {
+  const modalStars = document.querySelector('.stats-stars');
+  const modalTime = document.querySelector('.stats-time');
+  const modalMoves = document.querySelector('.stats-moves');
+  modalStars.innerHTML = starsStats.innerHTML;
+  modalTime.innerHTML = totalTime.innerHTML;
+  modalMoves.innerHTML = `Total moves: ${moves}`;
+}
+
+// Event delegation to restart the board from winning modal button
+modalButton.addEventListener('click', restartGame);
+
+// Event delegation to close the winning modal using the X
+modalCloseX.addEventListener('click', closeModal);
+
+// Function to restart the game
+function restartGame() {
+  modal.setAttribute('style', 'display: none;');
+  matchedCards = 0;
+  cardsList = [];
+  clearInterval(timeStart);
+  clockOn = false;
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  hoursLabel.innerText = '00';
+  minutesLabel.innerText = '00';
+  secondsLabel.innerText = '00';
+  clearInterval(timeStart);
+  moves = 0;
+  movesCount.innerText = 'No moves';
+  stars[9].innerHTML = '<i class="fas fa-star">';
+  stars[7].innerHTML = '<i class="fas fa-star">';
+  stars[5].innerHTML = '<i class="fas fa-star">';
+  stars[3].innerHTML = '<i class="fas fa-star">';
+  stars[1].innerHTML = '<i class="fas fa-star">';
+  const cards = document.querySelectorAll('.deck li');
+  for (let card of cards) {
+    card.className = 'card';
+  }
+  randomDeck();
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -70,25 +227,3 @@ function shuffle(array) {
 
     return array;
 }
-
-// Click moves
-function clickNum() {
-    moves++;
-    if (moves === 1) {
-        movesCounter.innerHTML = '1 move';
-    } else {
-        movesCounter.innerHTML = `${moves} moves`;
-    }
-}
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
